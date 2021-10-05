018bb632a024cdd045630f8f95aaf34344888d1a
Manmohan506 <manmohan@zelarsoft.com>
2021-10-05 09:08:01 +0530

    hh

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,11 +4,11 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "manmohan" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "MANMOHAN"
+    Name = "terraform"
   }
 }
