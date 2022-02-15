5ca975cc67e155db345386d46cd55e865e60570d
Manmohan506 <82638717+Manmohan506@users.noreply.github.com>
2022-02-16 00:31:15 +0530

    done

--- vpc/main.tf
+++ vpc/main.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  aws_access_key = AKIARSTSBWMIV225JBW2
+  aws_secret_key = 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
 }
   required_providers {
     aws = {
